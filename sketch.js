00fabc5d1a27c115ece59125d335ab2a25ccacca
Lucas Vitor <93093445+vitrlc@users.noreply.github.com>
2021-11-26 15:38:21 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ var garden,rabbit;
 var gardenImg,rabbitImg;
 var apple, appleImg
 var leaf, leafImg
+
 function preload(){
   gardenImg = loadImage("garden.png");
   rabbitImg = loadImage("rabbit.png");
@@ -12,15 +13,14 @@ function preload(){
 function setup(){
   
   createCanvas(400,400);
-  
-// Moving background
-garden=createSprite(200,200);
-garden.addImage(gardenImg);
 
-//creating boy running
-rabbit = createSprite(180,340,30,30);
-rabbit.scale =0.09;
-rabbit.addImage(rabbitImg);
+  garden=createSprite(200,200);
+  garden.addImage(gardenImg);
+
+
+  rabbit = createSprite(180,340,30,30);
+  rabbit.scale =0.09;
+  rabbit.addImage(rabbitImg);
 }
 
 
@@ -34,30 +34,41 @@ function draw() {
   if (frameCount % 80 == 0) {
   if (select_sprites  == 1) 
   { createApples(); }
-  else if  (select_sprites == 2) {         createApples(); }
-else {  createApples(); } }
+  
+    else if  
+      (select_sprites == 2)
+        createApples(); 
+    
+    else 
+      createApples();  
   drawSprites();
   
   var select_sprites = Math.round(random(1,3));
   if (frameCount % 100 == 0) {
   if (select_sprites  == 1) 
-  { createLeaves(); }
-  else if  (select_sprites == 5) {
+      { createLeaves(); }
+    
+    else if  
+    (select_sprites == 5){
     createLeaves(); }
-else {  createLeaves(); } }
+    
+    else 
+    {  createLeaves(); }
+      
   drawSprites();
 }
-function createApples() {
-apple=createSprite(random(50,350),40,10,10)
-apple.addImage(appleImg);
-apple.scale=0.1;
-apple.velocityY=5;
-apple.lifetime=150;
+ function createApples() {
+  apple=createSprite(random(50,350),40,10,10)
+  apple.addImage(appleImg);
+  apple.scale=0.1;
+  apple.velocityY=5;
+  apple.lifetime=150;
 }
+
 function createLeaves() {
-leaf=createSprite(random(50,350),40,10,10)
-leaf.addImage(leafImg);
-leaf.scale=0.1;
-leaf.velocityY=5;
-leaf.lifetime=150;
+  leaf=createSprite(random(50,350),40,10,10)
+  leaf.addImage(leafImg);
+  leaf.scale=0.1;
+  leaf.velocityY=5;
+  leaf.lifetime=150;
 }
